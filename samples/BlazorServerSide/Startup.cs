d954e57b2fd38215279f3eb60aba0d5617237c71
Chris Sainty <chrissainty@users.noreply.github.com>
2019-06-13 21:22:55 +0100

    Upgrade to preview 6

--- samples/BlazorServerSide/Startup.cs
+++ samples/BlazorServerSide/Startup.cs
@@ -1,14 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BlazorServerSide.Data;
 
 namespace BlazorServerSide
 {
@@ -20,7 +13,6 @@ namespace BlazorServerSide
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
