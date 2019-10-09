ef7334bb8aa5babb44983a8695ec0a68299cbb1e
R. Natsuo <1655659+natsuo@users.noreply.github.com>
2019-10-10 00:00:07 +0200

    WIP - Adds IsActive to the client side example

---
+++ samples/Shared/ApiResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample.Shared
+{
+    public class ApiResult<T>
+    {
+        public T Data { get; set; }
+        public int Count { get; set; }
+        public int Page { get; set; }
+        public int TotalPages { get; set; }
+    }
+}
--- samples/Shared/Person.cs
+++ samples/Shared/Person.cs
@@ -18,5 +18,6 @@ namespace Sample.Shared
         public string FullName { get => Firstname + " " + Lastname; }
         public int Age { get; set; }
         public string Location { get; set; }
+        public bool IsActive { get; set; }
     }
 }
